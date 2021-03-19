a3439c5b65ac692ddb2830309638d1d56a38ce74
SergioVzqz <sergiovazag@gmail.com>
2021-03-19 14:29:56 -0600

    JSX Learning

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const element = document.createElement('h1');
+// element.innerText = 'Hola react..';
+// const container = document.getElementById('root');
+// container.appendChild(element)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const user = {
+  nombre: 'Sergio',
+  apellido: 'Vazquez',
+  avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
+}
+
+function getName(user){
+  return `${user.nombre} ${user.apellido}`
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {getName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+const element = (
+      <div>
+        <h1>{getGreeting(user)}</h1>
+        <img src={user.avatar} alt="avatar"/>
+      </div>)
+const container = document.getElementById('root')
+
+
+//Recibe dos parametros: Que elemento va a renderear y donde
+ReactDOM.render(element, container)
